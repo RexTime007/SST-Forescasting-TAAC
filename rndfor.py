17a2de7ff7e208a3b469c9777a731854493e24ba
RexTime007 <mirinrexy20@icloud.com>
2024-03-18 23:25:44 -0600

    Random Forest with plot

--- rndfor.py
+++ rndfor.py
@@ -1,14 +1,16 @@
 import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 
 # Supongamos que df es tu DataFrame con las columnas 'Date' y 'Temperature'
 df = pd.read_csv('LatLonFiltered.csv')
 df['Fecha'] = pd.to_datetime(df['Fecha'])
 
 # Configuración de la ventana deslizante
-window_size = 12  # 36 meses
-prediction_step = 6  # Predecir 6 meses adelante
+window_size = 12 # 36 meses
+prediction_step = 1  # Predecir 6 meses adelante
 train_start = 0
 train_end = train_start + window_size
 
@@ -47,8 +49,49 @@ resultados = pd.DataFrame({
     'Prediccion_SST': predictions
 })
 
-resultados.to_csv("output_rndForest1.csv")
-print(resultados)
+
+resultados.to_csv("output_rndForest1.csv", index=False)
+
+df_adjust = df[(df["Fecha"] >= '2020-01-01') & (df['Fecha'] < '2023-01-01')]
+
+df_adjust = df_adjust.reset_index(drop=True)
+
+df_adjust = pd.DataFrame(df_adjust)
+
+#df_adjust.to_csv("2020_2022.csv", index=False)
+
 # Evaluación del modelo
 mse = mean_squared_error(actual, predictions)
 print('Mean Squared Error:', mse)
+
+
+resultados = pd.read_csv("output_rndForest1.csv")
+# Asumiendo que data['SST'] son tus observaciones reales y y_ts_pred son tus predicciones.
+df_adjust = pd.read_csv("2020_2022.csv")
+
+resultados.insert(0, 'Fecha', df_adjust['Fecha'])
+
+print((resultados))
+"""# Convertir año y mes a una fecha (primer día de cada mes)
+sst_promedio_mensual['Fecha'] = pd.to_datetime(sst_promedio_mensual['Año'].astype(str) + '-' +
+                                               sst_promedio_mensual['Mes'].astype(str) + '-01')
+"""
+df_adjust['Fecha'] = pd.to_datetime(df_adjust['Fecha'])
+resultados['Fecha'] = pd.to_datetime(resultados['Fecha'])
+
+# Gráfico
+plt.figure(figsize=(12, 6))
+plt.plot(df_adjust['Fecha'], df_adjust['SST'], marker='o', linestyle='-', label='Observaciones Reales')
+plt.plot(df_adjust['Fecha'], resultados['Prediccion_SST'], marker='*', linestyle='-', label='Predicciones')
+plt.title('Temperatura Superficial del Mar (SST) Real vs Random Forest a 1 mes')
+plt.xlabel('Fecha')
+plt.ylabel('SST')
+plt.legend()
+
+# Formatear el eje x para mostrar cada año
+plt.gca().xaxis.set_major_locator(mdates.MonthLocator())
+plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y,%m'))
+plt.gcf().autofmt_xdate()  # Auto-rotar las fechas para que sean legibles
+plt.grid(True)
+plt.xticks(rotation=90)
+plt.show()
